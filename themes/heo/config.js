ab94d1c69aed722dca8db6ac3bdc8084bbb000c2
moliyy <100828684+moliyy@users.noreply.github.com>
2025-04-03 18:13:29 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '绮梦博客',
   HEO_HERO_TITLE_2: '分享好康的',
-  HEO_HERO_TITLE_3: 'https://notion-next-87ux.vercel.app/',
+  HEO_HERO_TITLE_3: '',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'NotionNext4.0 ',
   HEO_HERO_TITLE_LINK: 'https://notion-next-87ux.vercel.app/',
